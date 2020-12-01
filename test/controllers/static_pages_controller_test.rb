1d122b5a4bd9c4978ae550f6d46487d23e5cdecd
Ubuntu <ubuntu@ip-172-31-17-231.ap-northeast-1.compute.internal>
2020-12-01 06:18:17 +0000

    Add a full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Instagram App"
+    assert_select "title", "Instagram App"
   end
 
   test "should get search" do
