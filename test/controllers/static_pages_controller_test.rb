f905ad8b1d1db8754b32c7d566924e37126aa5f7
Ubuntu <ubuntu@ip-172-31-17-231.ap-northeast-1.compute.internal>
2020-12-01 06:07:05 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,26 +4,31 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Instagram App"
   end
 
   test "should get search" do
     get static_pages_search_url
     assert_response :success
+    assert_select "title", "Search | Instagram App"
   end
 
   test "should get notice" do
     get static_pages_notice_url
     assert_response :success
+    assert_select "title", "Notice | Instagram App"
   end
 
   test "should get mypage" do
     get static_pages_mypage_url
     assert_response :success
+    assert_select "title", "Mypage | Instagram App"
   end
 
   test "should get upload" do
     get static_pages_upload_url
     assert_response :success
+    assert_select "title", "Upload | Instagram App"
   end
 
 end
