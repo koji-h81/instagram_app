1aa265c2cd98dcb8c9aa58a363fd7d2d0c6785f7
Ubuntu <ubuntu@ip-172-31-17-231.ap-northeast-1.compute.internal>
2020-12-02 20:27:40 +0000

    add login flash

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: @params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       
     else
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
